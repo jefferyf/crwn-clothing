3a3dcc2a137d68955cc85402444b0c493dcc60e3
Jeff Miller <jeffery.f@gmail.com>
2020-04-30 10:18:07 -0500

    Added header component and styles

---
+++ src/components/header/header.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import './header.styles.scss';
+import { ReactComponent as Logo } from '../../assets/crown.svg';
+
+const Header = () => {
+  return (
+    <div className='header'>
+      <Link to="/" className='logoContainer'>
+        <Logo className="logo" />
+      </Link>
+      <div className='options'>
+        <Link to='/shop' className='option'>
+          Shop
+        </Link>
+        <Link to='/shop' className='option'>
+          Contact
+        </Link>
+      </div>
+    </div>
+  );
+}
+
+export default Header;
